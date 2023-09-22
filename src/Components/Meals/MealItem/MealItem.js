14445427f919287257215bd536e0cd4976fbee32
Leslie Cruz <lesliecruz2@my.unt.edu>
2023-09-21 22:00:23 -0500

    Worked on outputting cart items

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,8 +1,22 @@
 import classes from './MealItem.module.css';
 import MealItemForm from "./MealItemForm";
+import {useContext} from "react";
+import CartContext from "../../../Store/CartContext";
 
- const MealItem = props => {
+const MealItem = props => {
+
+    const cartCtx = useContext(CartContext);
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+
+        });
+    };
     return(
         <li className={classes.meal}>
             <div>
@@ -10,7 +24,7 @@ import MealItemForm from "./MealItemForm";
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div></div>
             <div>
-                <MealItemForm/>
+                <MealItemForm onAddToCart={addToCartHandler}/>
             </div>
         </li>
 
