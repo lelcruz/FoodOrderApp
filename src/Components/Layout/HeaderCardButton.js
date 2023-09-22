26a9336d35fa3640d3f0451c974699e0d832f1c6
Leslie Cruz <lesliecruz2@my.unt.edu>
2023-09-22 15:19:44 -0500

    Made items removable inside of cart

--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,15 +1,36 @@
-import { useContext} from "react";
+import { useContext, useEffect, useState} from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../Store/CartContext";
 const HeaderCardButton = props => {
+    const [btnHighlighted, setBtnHighlited] = useState(false);
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    const {items} = cartCtx;
+
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0);
+
+    const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0){
+            return;
+        }
+        setBtnHighlited(true);
+
+        const timer = setTimeout(() => {
+            setBtnHighlited(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return(
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
 
